81a00d8e18ae7e95fe95435589c9327decbc0c05
phyroza <phyroza@gmail.com>
2018-10-15 21:23:16 +0200

    Added question mark

--- src/com/tomslab/Main.java
+++ src/com/tomslab/Main.java
@@ -1,9 +1,9 @@
 package com.tomslab;
 
-public class Main {
-
-    public static void main(String[] args)
-    {
-        System.out.println("LOL.");
-    }
+public class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println("LOL?");
+  }
 }
