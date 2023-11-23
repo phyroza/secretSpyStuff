2aac2c074908aa9320cda817bd4c0f31f6a8d512
Tom <tomashromaniuk@gmail.com>
2023-11-23 20:09:27 +0100

    Chapter2/Exercise7

--- src/main/java/com/tomslabs/exercises/chapter2/Exercise5.java
+++ src/main/java/com/tomslabs/exercises/chapter2/Exercise5.java
@@ -1,11 +1,28 @@
 package com.tomslabs.exercises.chapter2;
 
 public class Exercise5 {
+    /**
+     * Class for representing Point on 2d plane
+     *
+     * @param x position on X axis
+     * @param y position on Y axis
+     */
     private record Point(double x, double y) {
+        /**
+         * move point around
+         * @param x x offset
+         * @param y y offset
+         * @return
+         */
         Point translate(double x, double y) {
             return new Point(x() + x, y() + y);
         }
 
+        /**
+         * Scales the point
+         * @param factor scaling facotr
+         * @return new scaled point
+         */
         Point scale(double factor) {
             return new Point(x() * factor, y() * factor);
         }
---
+++ src/main/java/com/tomslabs/exercises/chapter2/Exercise8.java
@@ -0,0 +1,5 @@
+package com.tomslabs.exercises.chapter2;
+
+public class Exercise8 {
+    //java doc added to 5, aint nobody got time for 6
+}
