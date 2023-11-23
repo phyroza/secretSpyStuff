71b5b6aacfd67f355898a4143c432176829da2d5
Tom <tomashromaniuk@gmail.com>
2023-11-23 19:45:55 +0100

    Chapter2/Exercise3

---
+++ src/main/java/com/tomslabs/exercises/chapter2/Exercise3.java
@@ -0,0 +1,8 @@
+package com.tomslabs.exercises.chapter2;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        // ad.1 tak Scanner nextInt()
+        // ad.2 tak metoda zwracajaca wynik w podanej referencji
+    }
+}
