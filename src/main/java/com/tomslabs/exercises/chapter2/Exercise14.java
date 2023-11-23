98d25c7650590804bde5a364bb44e9d2e6a90efa
Tom <tomashromaniuk@gmail.com>
2023-11-23 22:10:39 +0100

    Chapter2/Exercise14

--- src/main/java/com/tomslabs/exercises/chapter2/Exercise14.java
+++ src/main/java/com/tomslabs/exercises/chapter2/Exercise14.java
@@ -1,4 +1,14 @@
 package com.tomslabs.exercises.chapter2;
 
+import com.opencsv.CSVParser;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Exercise14 {
+    public static void main(String[] args) throws IOException {
+        CSVParser csvParser = new CSVParser();
+        String[] strings = csvParser.parseLine("lol,wut,meh");
+        System.out.println("strings = " + Arrays.toString(strings));
+    }
 }
--- src/main/java/com/tomslabs/exercises/chapter2/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package r01.r01_01;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
