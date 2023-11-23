edaec137ae791b8f3d5cb2fdd2db0be1807a047e
Tom <tomashromaniuk@gmail.com>
2023-11-23 22:37:31 +0100

    Chapter2/Exercise15

---
+++ src/main/java/com/tomslabs/exercises/chapter2/Exercise15.java
@@ -0,0 +1,43 @@
+package com.tomslabs.exercises.chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise15 {
+
+    private static class Network {
+        List<Member> members = new ArrayList<>();
+
+        public Member enroll(String name) {
+            Member newMember = this.new Member(name);
+            members.add(newMember);
+            return newMember;
+        }
+
+        public void unenroll(Member member) {
+            members.remove(member);
+        }
+
+        private class Member {
+            String name;
+            List<Member> friends;
+
+            public Member(String name) {
+                this.name = name;
+                this.friends = new ArrayList<>();
+            }
+
+            public void deactivate() {
+                Network.this.members.remove(this);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        var myNetwork = new Network();
+        Network.Member john = myNetwork.enroll("John");
+        john.deactivate();
+        Network.Member jane = myNetwork.enroll("Jane");
+        myNetwork.unenroll(jane);
+    }
+}
