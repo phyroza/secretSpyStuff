86f31593457b5e78e723bdf5f4734ede9c17dce5
Tom <tomashromaniuk@gmail.com>
2023-11-23 19:35:43 +0100

    Chapter2/Exercise1 touchups

---
+++ src/main/java/com/tomslabs/exercises/chapter2/Exercise2.java
@@ -0,0 +1,10 @@
+package com.tomslabs.exercises.chapter2;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        new Scanner(System.in).nextInt();
+        // modyfikujaca - postion i inne pola są modyfikowane.
+    }
+}
