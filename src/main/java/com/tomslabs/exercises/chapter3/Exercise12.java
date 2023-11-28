0dbef8cfa92fb01e5ccfe79ccb74a6538c153045
Tom <tomashromaniuk@gmail.com>
2023-11-28 15:28:01 +0100

    Chapter3/Exercise 12

---
+++ src/main/java/com/tomslabs/exercises/chapter3/Exercise12.java
@@ -0,0 +1,20 @@
+package com.tomslabs.exercises.chapter3;
+
+import java.io.File;
+
+public class Exercise12 {
+    private static File[] listFiles(File root, String extension) {
+        return root.listFiles(file -> file.isFile() && file.getName().endsWith(extension));
+    }
+
+    public static void main(String[] args) {
+        File root = new File("C:\\Windows");
+        String extension = ".exe";
+        File[] files = listFiles(root, extension);
+        for (File file : files) {
+            System.out.println("file = " + file);
+        }
+
+        // root & extension
+    }
+}
