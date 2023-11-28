0d0a47fed0eeb69e8354485469f75eeee8ed0fb3
Tom <tomashromaniuk@gmail.com>
2023-11-28 13:34:07 +0100

    Chapter3/Exercises 3 & 4

--- src/main/java/com/tomslabs/exercises/chapter3/Exercise3.java
+++ src/main/java/com/tomslabs/exercises/chapter3/Exercise3.java
@@ -1,12 +1,10 @@
 package com.tomslabs.exercises.chapter3;
 
 import javax.imageio.stream.ImageOutputStream;
-import java.lang.constant.Constable;
-import java.lang.constant.ConstantDesc;
 import java.util.Scanner;
 
 public class Exercise3 {
-    private final String string = new String(); //implements java.io.Serializable, Comparable<String>, CharSequence,
+    private final String string = ""; //implements java.io.Serializable, Comparable<String>, CharSequence,
     // Constable, ConstantDesc
     private final Scanner scanner = new Scanner(System.in); // Iterator<String>, Closeable
     private final ImageOutputStream imageOutputStream = null; // ImageInputStream, DataOutput
