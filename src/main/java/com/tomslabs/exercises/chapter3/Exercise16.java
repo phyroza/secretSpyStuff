f3914e2fdda653a1f2d8e447f4bfa53750fc96fd
Tom <tomashromaniuk@gmail.com>
2023-11-28 16:14:18 +0100

    Chapter3/Exercise 16

---
+++ src/main/java/com/tomslabs/exercises/chapter3/Exercise16.java
@@ -0,0 +1,11 @@
+package com.tomslabs.exercises.chapter3;
+
+import java.util.random.RandomGenerator;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        IntSequence randomInts = IntSequence.randomInts(0, 100);
+        for (int i = 0; i < 100; i++)
+            System.out.println(randomInts.next());
+    }
+}
--- src/main/java/com/tomslabs/exercises/chapter3/IntSequence.java
+++ src/main/java/com/tomslabs/exercises/chapter3/IntSequence.java
@@ -1,6 +1,9 @@
 package com.tomslabs.exercises.chapter3;
 
+import java.util.random.RandomGenerator;
+
 public interface IntSequence {
+
     boolean hasNext();
 
     int next();
@@ -35,5 +38,31 @@ public interface IntSequence {
                 return next;
             }
         };
+    }
+
+    static IntSequence randomInts(int low, int high) {
+        return new RandomSequence(low, high);
+    }
+
+    class RandomSequence implements IntSequence {
+        static RandomGenerator gen = RandomGenerator.getDefault();
+
+        private final int low;
+        private final int high;
+
+        public RandomSequence(int low, int high) {
+            this.low = low;
+            this.high = high;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public int next() {
+            return gen.nextInt(low, high);
+        }
     }
 }
