004efa41ab52bd8ba07d9cfb4d5de77819b324a6
Tom <tomashromaniuk@gmail.com>
2023-11-28 14:31:52 +0100

    Chapter3/Exercise 8

---
+++ src/main/java/com/tomslabs/exercises/chapter3/Exercise8.java
@@ -0,0 +1,22 @@
+package com.tomslabs.exercises.chapter3;
+
+import java.util.*;
+
+public class Exercise8 {
+    static void luckySort(ArrayList<String> strings, Comparator<String> comp) {
+        ArrayList<String> unsortedStrings = new ArrayList<>(strings);
+        strings.sort(comp);
+        int iterations = 0;
+        while (!unsortedStrings.equals(strings)) {
+            Collections.shuffle(unsortedStrings);
+            iterations++;
+        }
+        System.out.println("Sorted with shuffle iterations = " + iterations);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>(List.of("lalala", "blebleble", "zonk", "lol", "aaa", "wutwut", "ccc"));
+        luckySort(strings, Comparator.comparing(String::length));
+        luckySort(strings, Comparator.naturalOrder());
+    }
+}
