626dfd4bad3bdf0472206886a06edc09bea99e19
Tom <tomashromaniuk@gmail.com>
2023-12-03 15:20:25 +0100

    Chapter5/5

---
+++ src/main/java/com/tomslabs/exercises/chapter5/Exercise7.java
@@ -0,0 +1,5 @@
+package com.tomslabs.exercises.chapter5;
+
+public class Exercise7 {
+    // ex could be thrown in contructor, try with resources won't know about this and exception will have to be handled by invoking code
+}
