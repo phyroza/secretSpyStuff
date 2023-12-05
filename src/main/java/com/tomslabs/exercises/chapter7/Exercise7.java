69f810f1c0cc5d4e2eb720b471143916fbff8329
Tom <tomashromaniuk@gmail.com>
2023-12-05 14:40:01 +0100

    Chapter7/7

---
+++ src/main/java/com/tomslabs/exercises/chapter7/Exercise7.java
@@ -0,0 +1,42 @@
+package com.tomslabs.exercises.chapter7;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import static java.lang.System.Logger.Level.ERROR;
+
+public class Exercise7 {
+
+    static TreeMap<String, Integer> wordsMap = new TreeMap<>(Comparator.comparing(String::length));
+    private static final System.Logger logger = System.getLogger(Exercise7.class.getName());
+
+    public static void main(String[] args) {
+        URL resource = Exercise7.class.getResource("/text/wordsFile.txt");
+        if (resource != null) {
+            try (Scanner scanner = new Scanner(resource.openStream(), StandardCharsets.UTF_8)) {
+                while (scanner.hasNext()) {
+                    String word = scanner.next();
+                    Integer previousValue = wordsMap.putIfAbsent(word, 1);
+                    if (previousValue != null) {
+                        wordsMap.computeIfPresent(word, (s, v) -> v + 1);
+                    }
+
+                }
+            } catch (IOException e) {
+                logger.log(ERROR, "Could not open the file");
+            }
+
+
+            List<Map.Entry<String, Integer>> list = wordsMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).toList();
+            for (Map.Entry<String, Integer> entry : list) {
+                System.out.println(entry);
+            }
+        }
+    }
+}
