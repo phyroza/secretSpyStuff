9531dd92a2633ce2f9932ea9735ade199f562951
Tom <tomashromaniuk@gmail.com>
2023-12-05 22:57:14 +0100

    Chapter7/11

---
+++ src/main/java/com/tomslabs/exercises/chapter7/Exercise13.java
@@ -0,0 +1,13 @@
+package com.tomslabs.exercises.chapter7;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Exercise13 {
+    private static class Cache<K, V> extends LinkedHashMap<K, V> {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+            return true;
+        }
+    }
+}
