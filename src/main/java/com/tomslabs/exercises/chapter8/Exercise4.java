6ca7df3f28fb975f5e57eee7fc3a04e55c0c5bf4
Tom <tomashromaniuk@gmail.com>
2023-12-08 17:12:19 +0100

    Chapter8/3

---
+++ src/main/java/com/tomslabs/exercises/chapter8/Exercise4.java
@@ -0,0 +1,16 @@
+package com.tomslabs.exercises.chapter8;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        List<Long> tenRandomLongs = getRandomInfiniteStream(25214903917L, 11, (long) Math.pow(2, 48)).limit(10).toList();
+        System.out.println("tenRandomLongs = " + tenRandomLongs);
+    }
+
+    private static Stream<Long> getRandomInfiniteStream(long a, long c, long m) {
+        return Stream.iterate(13L, prev -> ((a * prev + c) % m));
+    }
+}
+
