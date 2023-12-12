0b1917a2cd111a60a5c944aec5e28152ded48348
Tom <tomashromaniuk@gmail.com>
2023-12-12 15:17:54 +0100

    Chapter9/8

---
+++ src/main/java/com/tomslabs/exercises/chapter9/Exercise7.java
@@ -0,0 +1,11 @@
+package com.tomslabs.exercises.chapter9;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Exercise7 {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+        // fuck this
+    }
+}
---
+++ src/main/java/com/tomslabs/exercises/chapter9/Exercise8.java
@@ -0,0 +1,45 @@
+package com.tomslabs.exercises.chapter9;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Collections;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        Path sourceDataPath = Paths.get("C:/training");
+        Path resultingZipPath = Paths.get("C:/tmp", sourceDataPath.getFileName().toString() + ".zip");
+        zipDirectory(sourceDataPath, resultingZipPath);
+    }
+
+    private static void zipDirectory(Path sourcePath, Path zipPath) {
+        URI zipUri;
+        try {
+            zipUri = new URI("jar", zipPath.toUri().toString(), null);
+            Files.deleteIfExists(zipPath);
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (FileSystem zipFileSystem = FileSystems.newFileSystem(zipUri, Collections.singletonMap("create", true))) {
+            Files.walk(sourcePath).filter(p -> !p.equals(sourcePath)).forEach(p -> {
+                        try {
+                            Path relativeFilePath = sourcePath.relativize(p);
+                            System.out.print(p + "->");
+                            System.out.println(zipUri + "/" + relativeFilePath);
+                            Files.copy(p, zipFileSystem.getPath(relativeFilePath.toString()), StandardCopyOption.REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
