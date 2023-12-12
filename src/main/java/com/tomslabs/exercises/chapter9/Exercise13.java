eb5c36f9897c225644654a012a3a1a9ded75cfeb
Tom <tomashromaniuk@gmail.com>
2023-12-12 19:42:09 +0100

    Chapter9/13

---
+++ src/main/java/com/tomslabs/exercises/chapter9/Exercise13.java
@@ -0,0 +1,42 @@
+package com.tomslabs.exercises.chapter9;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Exercise13 {
+    private static class Smth implements Serializable {
+        String name;
+        int furyFactor = 0;
+        transient String soul;
+
+        public Smth(String name, int furyFactor, String soulElement) {
+            this.name = name;
+            this.furyFactor = furyFactor;
+            soul = soulElement;
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Smth a = new Smth("MyLife", Integer.MAX_VALUE, "joyful");
+
+        Smth clonedObject = cloneBySerialize(a);
+
+        System.out.println(a);
+        System.out.println(clonedObject);
+    }
+
+    private static Smth cloneBySerialize(Smth a) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream objectBuffer = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(objectBuffer);
+        oos.writeObject(a);
+
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(objectBuffer.toByteArray()));
+        Smth readObject = (Smth) ois.readObject();
+
+        return readObject;
+    }
+}
