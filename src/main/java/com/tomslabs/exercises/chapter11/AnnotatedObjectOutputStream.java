20ab9dc1e61fd9ba1df7bacc1b38ff4c386f0a3f
Tom <tomashromaniuk@gmail.com>
2023-12-15 14:21:42 +0100

    Chapter11/2 obj1

--- src/main/java/com/tomslabs/exercises/chapter11/AnnotatedObjectOutputStream.java
+++ src/main/java/com/tomslabs/exercises/chapter11/AnnotatedObjectOutputStream.java
@@ -10,12 +10,13 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+import static com.tomslabs.exercises.chapter11.Serializable.SerializationFormat.TEXT;
+
 public class AnnotatedObjectOutputStream {
     public static final Charset charset = StandardCharsets.UTF_8;
-    public static final String lineSeprator = System.getProperty("line.separator");
+    public static final String lineSeparator = System.getProperty("line.separator");
     public static final String classMarker = ">>>";
     OutputStream out;
-
     Serializable.SerializationFormat format;
     private Base64.Encoder encoder = Base64.getEncoder();
 
@@ -24,43 +25,52 @@ public class AnnotatedObjectOutputStream {
         this.format = format;
     }
 
-    public void writeObject(Object object) throws NotSerializableException {
+    public void writeObject(Object object) throws IOException {
         Class<?> aClass = object.getClass();
         if (!checkIfSerializable(object))
             throw new NotSerializableException(aClass.getName());
 
         Serializable annotation = aClass.getAnnotation(Serializable.class);
-        Serializable.SerializationFormat classFormat = annotation.format();
+        Serializable.SerializationFormat classFormat = annotation != null ? annotation.format() : TEXT;
 
         if (classFormat != format)
             throw new IllegalArgumentException("Invalid class serialization format for this object output stream");
 
-        if (format.equals(Serializable.SerializationFormat.TEXT)) {
-            try (Writer writer = new OutputStreamWriter(out, StandardCharsets.UTF_8)) {
-                if (format.equals(Serializable.SerializationFormat.TEXT)) {
-                    writer.write(classMarker + aClass.getCanonicalName() + lineSeprator);
+        if (format.equals(TEXT)) {
+            {
+                Writer writer = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+                if (format.equals(TEXT)) {
+                    writer.write(classMarker + aClass.getCanonicalName() + lineSeparator);
                 }
                 Field[] fields = aClass.getDeclaredFields();
                 for (Field field : fields) {
                     try {
-                        String value = field.get(object).toString();
-                        byte[] encodedValue = encoder.encode(value.getBytes(charset));
-                        writer.write(field.getName() + ":" + new String(encodedValue, charset));
-                        writer.write(lineSeprator);
+                        if (field.getType().isPrimitive())
+                            writePrimitiveField(object, field, writer);
+                        else {
+                            field.setAccessible(true);
+                            writeObject(field.get(object));
+                        }
                     } catch (IOException | IllegalAccessException e) {
                         throw new RuntimeException(e);
                     }
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
         }
 
 
     }
 
+    private void writePrimitiveField(Object object, Field field, Writer writer) throws IllegalAccessException, IOException {
+        String value = field.get(object).toString();
+        byte[] encodedValue = encoder.encode(value.getBytes(charset));
+        writer.write(field.getName() + ":" + new String(encodedValue, charset));
+        writer.write(lineSeparator);
+    }
+
     private boolean checkIfSerializable(Object object) {
-        Class<?> aClass = object.getClass();
-        return aClass.getAnnotation(Serializable.class) != null;
+        return true;
+//        Class<?> aClass = object.getClass();
+//        return aClass.getAnnotation(Serializable.class) != null;
     }
 }
